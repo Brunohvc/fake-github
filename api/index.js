dd74d82c289423ea005cab66b2eb8cfee9ed580f
Bruno Henrique <brunohvcarvalho@gmail.com>
2021-11-24 21:10:31 -0300

    init

---
+++ api/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+const reposData = {};
+
+// Home page route.
+router.get('/:userName', function (req, res) {
+    const user = req?.params?.userName;
+    let checkHouer = diff_hours(reposData?.[user]?.addedDate) >= 1;
+    if (reposData[user] && checkHouer) {
+        let userData = { ...reposData[user] };
+        delete userData.repositories;
+        delete userData.addedDate;
+        delete userData.addedDateRepositories;
+        res.json(userData);
+    } else {
+        fetch('https://api.github.com/users/' + user)
+            .then(function (resultado) {
+                resultado.json().then(function (data) {
+                    reposData[user] = data;
+                    reposData[user].addedDate = new Date();
+                    res.json(data);
+                });
+            }).catch(function (erro) {
+                res.status(400).json('Falar com Prof.');
+            });
+    }
+});
+
+router.get('/:userName/repos', function (req, res) {
+    const user = req?.params?.userName;
+    let checkHouer = diff_hours(reposData?.[user]?.addedDateRepositories) >= 1;
+    if (reposData[user]?.actualRepositories && checkHouer) {
+        let userData = [...(reposData[user]?.actualRepositories || [])];
+        delete userData.repositories;
+        delete userData.addedDate;
+        delete userData.addedDateRepositories;
+        res.json(userData);
+    } else {
+        fetch('https://api.github.com/users/' + user + '/repos')
+            .then(function (resultado) {
+                resultado.json().then(function (data) {
+                    if (!reposData[user]) reposData[user] = {};
+                    reposData[user].actualRepositories = data;
+                    reposData[user].addedDateRepositories = new Date();
+                    res.json(data);
+                });
+            }).catch(function (erro) {
+                res.status(400).json('Falar com Prof.');
+            });
+    }
+});
+
+function diff_hours(dt2, dt1) {
+    if (!dt2 || !dt1) return;
+    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= (60 * 60);
+    return Math.abs(Math.round(diff));
+}
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const api = require('./api/index');
+console.clear();
+
+function logger(req, res, next) {
+    console.log('URL:', req.url);
+    next();
+}
+
+function notFound(req, res) {
+    res.status(404).send('Not Found Api!');
+}
+
+app.use(logger);
+app.use('/api/', api);
+app.use(notFound);
+
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
